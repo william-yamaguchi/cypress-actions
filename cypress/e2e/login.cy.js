a33dd65545c7fd434f04239ddcb34db566603403
william-yamaguchi <140436286+william-yamaguchi@users.noreply.github.com>
2025-04-29 10:42:25 -0300

    Update login.cy.js
    simulando dois bugs

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,12 +23,12 @@ describe('Login', () => {
     it('senha incorreta', () => {
         cy.login('qa', 'teste')
         cy.get(toast)
-            .should('have.text', 'Oops! Credenciais inválidas :(')
+            .should('have.text', 'Oops! Senha incorreta :(')
     })
 
     it('com sucesso', () => {
         cy.login('qa', 'xperience')
         cy.get(modalContent)
-            .should('have.text', 'Suas credenciais são válidas :)')
+            .should('have.text', 'Suas credenciais estão corretas :)')
     })
 })
